2236a3900db49eda9b1b0d35137b27297ac5df5e
Michael Gerstmann <michael.gerstmann@fitx.de>
2018-01-02 11:52:49 +0100

    split error message handling from example script

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 import { Form } from './form.js';
-
-/**
- * Handle Error Message Translation, Translate Error Messages via given Key
- * @param {String} value 
- */
-async function translate(value) {
-  let data = await fetch('validation-messages.json');
-  let parsedData = await data.json();
-
-  return parsedData[value] || value;
-}
+import { displayErrorMessage, removeErrorMessage } from './error-messages.js';
 
 const form1 = document.getElementById('form-1');
 const formHandling = new Form(form1);
@@ -65,32 +55,6 @@ function handleInputKeyDown(event) {
       handleFocusonValid(field, inputs);
     }
   }
-}
-
-/**
- * Show Error Messages
- * @param {HTMLElement} target 
- * @param {*} message 
- */
-async function displayErrorMessage(target, message = null) {
-  const translatedMessage = await translate(message);
-  const errorElement = `<span class="error-message">${translatedMessage}</span>`;
-  const element = target;
-
-  if (element.querySelectorAll('.error-message').length === 0) {
-    element.insertAdjacentHTML('beforeend', errorElement);
-  } else {
-    element.querySelector('.error-message').innerText = translatedMessage;
-  }
-}
-
-/**
- * Remove Error Messages
- * @param {HTMLElement} target 
- */
-async function removeErrorMessage(target) {
-  const errorElement = target.querySelector('.error-message');
-  errorElement.remove();
 }
 
 

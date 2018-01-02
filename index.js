5e2596df38a589b8e5d304698d84b99d9e60e71e
Michael Gerstmann <michael.gerstmann@fitx.de>
2018-01-02 14:37:55 +0100

    example for multiple form validations

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 import { Form } from './form.js';
 import { eventInit, handleSubmit, handleInputChange, handleInputKeyDown } from './event-handling.js';
 
-const form1 = document.getElementById('form-1');
-const formHandling = new Form(form1);
+const $form = document.getElementById('form-1');
+const form = new Form($form);
 
+const $form2 = document.getElementById('form-2');
+const form2 = new Form($form2);
+
+/**
+ * Global init for all Form Events
+ */
 eventInit();
 
 /* const helperFunc = (event, formClass) => {
   handleSubmit(event, formClass);
 }; */
 
-for (const submitButton of document.querySelectorAll('.js-submit')) {
-  submitButton.addEventListener('click', handleSubmit.bind(formHandling));
+
+/**
+ * Handle Form 1
+ */
+for (const submitButton of $form.querySelectorAll('.js-submit')) {
+  submitButton.addEventListener('click', handleSubmit.bind(form));
 }
 
-for (const field of formHandling.getFieldsToValidate()) {
-  field.addEventListener('keydown', handleInputKeyDown.bind(formHandling));
-  field.addEventListener('change', handleInputChange.bind(formHandling));
+for (const field of form.getFieldsToValidate()) {
+  field.addEventListener('keydown', handleInputKeyDown.bind(form));
+  field.addEventListener('change', handleInputChange.bind(form));
 }
 
-/* for (let i = 0; i < formHandling.getFieldsToValidate().length; i += 1) {
-  // formHandling.getFieldsToValidate()[i].addEventListener('change', handleInputChange);
-  formHandling.getFieldsToValidate()[i].addEventListener('keydown', handleInputKeyDown);
-} */
+
+/**
+ * Handle Form 2
+ */
+// Example for multiple Submit Buttons, include Buttons outside from scoped form
+for (const submitButton of [...$form2.querySelectorAll('.js-submit'), ...document.querySelectorAll(`button[form="${$form2.id}"]`)]) {
+  submitButton.addEventListener('click', handleSubmit.bind(form2));
+}
+
+for (const field of form2.getFieldsToValidate()) {
+  field.addEventListener('keydown', handleInputKeyDown.bind(form2));
+  field.addEventListener('change', handleInputChange.bind(form2));
+}

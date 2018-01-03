c9f75ffc069675ad5af5c477cc2fe77005a104af
Frank Holder <frank.holder@fitx.de>
2018-01-03 12:26:07 +0100

    clean code style

--- tests/rules/validate-uuid.test.js
+++ tests/rules/validate-uuid.test.js
@@ -3,22 +3,22 @@ import validate from '../../src/rules/validate-uuid';
 
 describe('UUID Validation', () => {
   it('should return false if value is empty', () => {
-    let result = validate('');
+    const result = validate('');
     expect(result.isValid).to.be.false;
   });
 
-  it('should return false if provided value is not a string', function() {
-    let result = validate(42);
+  it('should return false if provided value is not a string', () => {
+    const result = validate(42);
     expect(result.isValid).to.be.false;
   });
 
-  it('should return true if provided value passes the regex pattern', function() {
-    let result = validate('550e8400-e29b-11d4-a716-446655440000');
+  it('should return true if provided value passes the regex pattern', () => {
+    const result = validate('550e8400-e29b-11d4-a716-446655440000');
     expect(result.isValid).to.be.true;
   });
 
-  it('should return false if provided value does not passes the regex pattern', function() {
-    let result = validate('zzzzzZZZZZzzzzz');
+  it('should return false if provided value does not passes the regex pattern', () => {
+    const result = validate('zzzzzZZZZZzzzzz');
     expect(result.isValid).to.be.false;
   });
 });

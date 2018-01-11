7a26cdfb8443032e06001f598fd2be00b1899664
Michael Gerstmann <michael.gerstmann@fitx.de>
2018-01-11 17:30:41 +0100

    implement radio field validation

--- src/error-messages.js
+++ src/error-messages.js
@@ -28,33 +28,36 @@ async function translate(value, options = defaultTranslationOptions) {
 
 /**
  * Show Error Messages
- * @param {HTMLElement} target
+ * @param {HTMLElement} targets
  * @param {*} message
  * @param {Object|Boolean} needTranslation - Initial False or Object
  */
-export async function displayErrorMessage(target, message = null, needTranslation = false) {
+export async function displayErrorMessage(targets, message = null, needTranslation = false) {
   let errorMessage = message;
   if (needTranslation) {
     errorMessage = await translate(message, needTranslation);
   }
 
   const errorElement = `<span class="error-message">${errorMessage}</span>`;
-  const element = target;
-
-  if (element.querySelectorAll('.error-message').length === 0) {
-    element.insertAdjacentHTML('beforeend', errorElement);
-  } else {
-    element.querySelector('.error-message').innerText = errorMessage;
+  
+  for (const element of targets) {
+    if (element.querySelectorAll('.error-message').length === 0) {
+      element.insertAdjacentHTML('beforeend', errorElement);
+    } else {
+      element.querySelector('.error-message').innerText = errorMessage;
+    }
   }
 }
 
 /**
  * Remove Error Messages
- * @param {HTMLElement} target
+ * @param {HTMLElement} targets
  */
-export async function removeErrorMessage(target) {
-  const errorElement = target.querySelector('.error-message');
-  if (errorElement) {
-    errorElement.remove();
+export async function removeErrorMessage(targets) {
+  for (const target of targets) {
+    const errorElement = target.querySelector('.error-message');
+    if (errorElement) {
+      errorElement.remove();
+    }
   }
 }

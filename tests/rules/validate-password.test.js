5f99c096c2f4aff182ab8fd69afcac6e716109dd
Frank Holder <frank.holder@fitx.de>
2018-04-05 17:25:45 +0200

    add more mock cases

--- tests/rules/validate-password.test.js
+++ tests/rules/validate-password.test.js
@@ -1,19 +1,32 @@
 import { expect } from 'chai';
 import validate from '../../src/rules/validate-password';
 
+const mock = [
+  '12345678',
+  '-.,T34Fkp0)',
+  'test123456789'];
+
+const emptyMock = ['', '', ''];
+
+const invalidmMock = [
+  '1234567',
+  'ab4!as2',
+  '-.,[1K',
+];
+
+const checkPassword = (element, index, array) => validate(element).isValid === true;
+
 describe('Password Validation', () => {
-  it('should return true for min 8 characters', () => {
-    const result = validate('password123');
-    expect(result.isValid).to.be.true;
+  it('should return true if provided value passes the length pattern', () => {
+    expect(mock.every(checkPassword)).to.be.true;
   });
 
-  it('should return false for less than 8 characters', () => {
-    const result = validate('abc');
-    expect(result.isValid).to.be.false;
+  it('should return false if value is empty', () => {
+    expect(emptyMock.every(checkPassword)).to.be.false;
   });
 
-  it('should return false if email value is empty', () => {
-    const result = validate('');
-    expect(result.isValid).to.be.false;
+  it('should return false for invalid Password values', () => {
+    expect(invalidmMock.every(checkPassword)).to.be.false;
   });
 });
+

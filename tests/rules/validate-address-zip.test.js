841b3497cd38aa10583143f8bf3ef32ce8e6cf5e
Frank Holder <fwholder@gmail.com>
2018-08-22 11:46:02 +0200

    add negative test

--- tests/rules/validate-address-zip.test.js
+++ tests/rules/validate-address-zip.test.js
@@ -1,7 +1,5 @@
 import { expect } from 'chai';
 import validate from '../../src/rules/validate-address-zip';
-import {info} from "../../src/rules/messages";
-import is from "../../src/rules/valid-object";
 
 describe('Address Zip Validation', () => {
   it('should return false if value is empty', () => {
@@ -16,34 +14,11 @@ describe('Address Zip Validation', () => {
     expect(validate('10247').isValid).to.be.true;
   });
 
+  it('should return false for a non valid string', () => {
+    expect(validate('1024789112345').isValid).to.be.false;
+  });
+
   it('should parse to string for a non string value', () => {
     expect(validate(10247).isValid).to.be.true;
   });
 });
-
-
-
-
-/*
-export default function validateAddressZip(zip) {
-  const zipValue = zip;
-
-  if (!zipValue) {
-    return info('zipIsRequired', '', is.INVALID);
-  }
-
-  if (typeof zipValue !== 'string') {
-    return info('zipWrongPattern', 'zipValue', is.INVALID);
-  }
-
-  if (zipValue.length !== zipValue.trim().length) {
-    return info('zipLengthIsIncorrect', zipValue, is.INVALID);
-  }
-
-  if (!zipValue.trim().match(/^([0]{1}[1-9]{1}|[1-9]{1}[0-9]{1})[0-9]{3}$/)) {
-    return info('zipWrongPattern', zipValue, is.INVALID);
-  }
-
-  return info('', '', is.VALID);
-}
-*/
